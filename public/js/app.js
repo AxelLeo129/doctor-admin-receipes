fd5fd9cdc5287d8ffeed2bc3d7c563efe62807ed
Administrador Merke <merke@s2.merke.net>
2020-01-17 03:06:43 -0500

    resolviendo conflicto del servidor

--- public/js/app.js
+++ public/js/app.js
@@ -56575,9 +56575,9 @@ vue__WEBPACK_IMPORTED_MODULE_0___default.a.use(vuesax__WEBPACK_IMPORTED_MODULE_1
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/pdaniels/Desarrollo/laravel/pharmazone.app/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /home/pdaniels/Desarrollo/laravel/pharmazone.app/resources/sass/app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! /home/pdaniels/Desarrollo/laravel/pharmazone.app/resources/assets/css/main.css */"./resources/assets/css/main.css");
+__webpack_require__(/*! /var/www/pharmazone.app/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /var/www/pharmazone.app/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /var/www/pharmazone.app/resources/assets/css/main.css */"./resources/assets/css/main.css");
 
 
 /***/ })
